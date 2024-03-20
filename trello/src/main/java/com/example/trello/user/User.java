043e345aec1872aca67b4389da8bafc8f9bad43f
beunchoi <byungeun0228@gmail.com>
2024-03-20 11:49:27 +0900

    ♻️User 엔티티 수정

--- trello/src/main/java/com/example/trello/user/User.java
+++ trello/src/main/java/com/example/trello/user/User.java
@@ -11,22 +11,42 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-
 @Entity
 @Table(name = "user")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
 
-    @Column(nullable = false)
-    private String username;
+	@Column(nullable = false)
+	private String username;
 
-    @Column(nullable = false)
-    private String email;
+	@Column(nullable = false)
+	private String email;
 
-    @Column(nullable = false)
-    private String password;
+	@Column(nullable = false)
+	private String password;
 
+	@Column(nullable = false)
+	private String userInfo = "자기소개 해주세요";
+
+	@Column(nullable = false)
+	private boolean active = true;
+
+	public User(SignupRequestDto request, String password) {
+		this.username = request.getUsername();
+		this.email = request.getEmail();
+		this.password = password;
+	}
+
+	public void updateProfile(ProfileRequestDto request) {
+		this.username = request.getUsername();
+		this.email = request.getEmail();
+		this.userInfo = request.getUserInfo();
+	}
+
+	public void inActiveUser() {
+		this.active = false;
+	}
 }
