3a5680cc05ef55574006ed304d4fb57ce0be06e0
JooJuneHo <jw059@naver.com>
2024-03-21 11:41:58 +0900

    :recycle: 사용하지 않는 filter 제거

--- trello/src/main/java/com/example/trello/columnList/entity/columnList.java
+++ trello/src/main/java/com/example/trello/columnList/entity/columnList.java
@@ -1,11 +1,15 @@
-package com.example.trello.columnList;
+package com.example.trello.columnList.entity;
 
 
+import com.example.trello.board.entity.Board;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,13 +19,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 @Entity
-@Table(name = "column")
+@Table(name = "columnList")
 public class columnList {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long column_id;
 
-
     @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
+    private Integer sequence;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
 }
