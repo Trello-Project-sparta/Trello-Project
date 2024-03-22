01d6b9f5257701ab3fb8ee4d5d5f3cda5cce0d4d
JooJuneHo <jw059@naver.com>
2024-03-22 17:05:05 +0900

    :sparkles: bulk insert 쿼리 개선

--- trello/src/main/java/com/example/trello/userBoard/controller/UserBoardController.java
+++ trello/src/main/java/com/example/trello/userBoard/controller/UserBoardController.java
@@ -1,10 +1,12 @@
 package com.example.trello.userBoard.controller;
 
+import com.example.trello.security.UserDetailsImpl;
 import com.example.trello.userBoard.dto.UserBoardRequestDto;
 import com.example.trello.userBoard.dto.UserBoardResponseDto;
 import com.example.trello.userBoard.service.UserBoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,17 +23,19 @@ public class UserBoardController {
 
     @PutMapping("/{boardId}/collaborators")
     public ResponseEntity<UserBoardResponseDto> addCollaborator(@PathVariable Long boardId,
-        @RequestBody UserBoardRequestDto userBoardRequestDto) {
+        @RequestBody UserBoardRequestDto userBoardRequestDto, @AuthenticationPrincipal
+    UserDetailsImpl userDetails) {
         UserBoardResponseDto userBoard = userBoardService.addCollaborator(boardId,
-            userBoardRequestDto);
+            userBoardRequestDto, userDetails.getUser());
         return ResponseEntity.ok().body(userBoard);
     }
 
     @DeleteMapping("/{boardId}/collaborators")
     public ResponseEntity<UserBoardResponseDto> deleteCollaborator(@PathVariable Long boardId,
-        @RequestBody UserBoardRequestDto userBoardRequestDto) {
+        @RequestBody UserBoardRequestDto userBoardRequestDto, @AuthenticationPrincipal
+    UserDetailsImpl userDetails) {
         UserBoardResponseDto userBoardResponseDto = userBoardService.deleteCollaborator(boardId,
-            userBoardRequestDto);
+            userBoardRequestDto, userDetails.getUser());
         return ResponseEntity.ok().body(userBoardResponseDto);
     }
 }
